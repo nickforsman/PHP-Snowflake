9cc295b66e90981f12b4b58ef9d620042fe55074
nickforsman <niclas.forsman@outlook.com>
2015-07-02 21:08:29 +0300

    - refactoring view class

--- src/View.php
+++ src/View.php
@@ -5,14 +5,14 @@ namespace Snowflake;
 class View 
 {
 
-	public $directory = 'resources/views/';
+	public $templateDirectory = 'resources/views/';
 
 	public function render($template, array $data = null) 
 	{
-		$template = $this->directory . $template;
+		$template = $this->templateDirectory . $template;
 
 		if ( ! file_exists($template)) {
-			throw new \InvalidArgumentException("The template $template was not found in $this->directory");
+			throw new \InvalidArgumentException("The template $template was not found in $this->templateDirectory");
 		} else {
 			extract($data);
 			ob_start();
