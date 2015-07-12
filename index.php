0c3bdc2db4941d63aa0192d820f688bbf8b4bc99
nickforsman <niclas.forsman@outlook.com>
2015-07-13 00:19:01 +0300

    - .

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new Snowflake\Snowflake();
+
+$app->config = require_once __DIR__.'/resources/main.php';
+
+$app->get('/home', [], function() use ($app) {
+	$data = ['name' => 'John Cena'];
+	$app->render('hello.php', $data);
+})
+->get('/contact', ['controller' => 'HomeController@index'])
+->put('/about', [], function() {
+	echo "Hello";
+});
+
+$app->setRouter(new Snowflake\Routing\Router($app->getRegisteredRoutes()));
+
+$app->start();
