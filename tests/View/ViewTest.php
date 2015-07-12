2bacc5a445dd6b5af901b80f784c7fb9f9bd1129
nickforsman <niclas.forsman@outlook.com>
2015-07-13 00:06:42 +0300

    - refactored view test

--- tests/View/ViewTest.php
+++ tests/View/ViewTest.php
@@ -8,14 +8,14 @@ class ViewTest extends PHPUnit_Framework_TestCase
 
 	public function setUp() 
 	{
-		$this->view = new View;
+		$this->view = new View(__DIR__ . '/');
 	}
 
 	public function testRendersTemplateIfExists() 
 	{
 		$data = ['name' => 'John'];
 
-		$this->view->render('hello.php', $data);
+		$this->view->render('test.php', $data);
 	}
 
 	/**
